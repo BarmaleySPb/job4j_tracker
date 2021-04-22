c9cf9e68e90d5e4658b1e8ce9087d25dae849023
Evgeny <e.g.zakharov@gmail.com>
2021-04-23 00:50:41 +0300

    1.8. Состояние объекта [#519767]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -1,4 +1,21 @@
 package ru.job4j.oop;
 
 public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.load;
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(20);
+        Battery toyMachine = new Battery(0);
+        battery.exchange(toyMachine);
+        System.out.println("Charge of toyMachine is: " + toyMachine.load);
+    }
 }
