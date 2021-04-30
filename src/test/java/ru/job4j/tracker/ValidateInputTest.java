36ec437f02bc14ac0e0743ebddd0ce1080c0a0ee
Evgeny <e.g.zakharov@gmail.com>
2021-04-30 15:58:21 +0300

    2.1. Тест на ValidateInput[311887#519736]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -32,23 +32,26 @@ public class ValidateInputTest {
     @Test
     public void whenLotValidInput() {
         Output out = new StubOutput();
-        String[] array = {"1", "3", "6", "2"};
-        Input in = new StubInput(array);
-        for (String s : array) {
-            ValidateInput input = new ValidateInput(out, in);
-            int selected = input.askInt("Select: ");
-            assertThat(selected, is(Integer.valueOf(s)));
-        }
+        Input in = new StubInput(
+                new String[] {"1", "2", "3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int first = input.askInt("Select: ");
+        assertThat(first, is(1));
+        int second = input.askInt("Select: ");
+        assertThat(second, is(2));
+        int third = input.askInt("Select: ");
+        assertThat(third, is(3));
     }
 
     @Test
     public void whenNegativeInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"-2", "1"}
+                new String[] {"-2"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Select: ");
-        assertThat(selected, is(1));
+        assertThat(selected, is(-2));
     }
 }
