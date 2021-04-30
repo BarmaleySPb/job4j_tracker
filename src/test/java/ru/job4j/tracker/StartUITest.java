c0c2bc7fe9b449072d6d8408b9e7a7ffb9e68999
Evgeny <e.g.zakharov@gmail.com>
2021-04-30 11:43:30 +0300

    1.1. Тесты на StartUI.[223313#519735]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -160,4 +160,26 @@ public class StartUITest {
                         + "1. Exit program" + System.lineSeparator()
         ));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"/* Пункты меню: неверный, верный. */}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                "Menu." + ln
+                        + "0. Exit program" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu." + ln
+                        + "0. Exit program" + ln
+                )
+        );
+    }
 }
