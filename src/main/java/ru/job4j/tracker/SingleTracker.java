79526c99b972939cad2df002bd9945885ab303d6
Evgeny <e.g.zakharov@gmail.com>
2021-05-03 10:57:07 +0300

    2. Singleton [#519746]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -4,6 +4,9 @@ public final class SingleTracker {
 
     private static Tracker tracker = null;
 
+    private SingleTracker() {
+    }
+
     public static Tracker getTracker() {
         if (tracker == null) {
             tracker = new Tracker();
