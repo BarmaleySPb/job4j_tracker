07f48bd05710b91af5900ed094787e8ba318309c
Evgeny <e.g.zakharov@gmail.com>
2021-05-07 13:30:29 +0300

    3. Банковские переводы [#519692]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,57 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        List<Account> accounts = users.get(findByPassport(passport));
+        if (!accounts.contains(account)) {
+            accounts.add(account);
+            users.put(findByPassport(passport), accounts);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        List<Account> accounts = users.get(findByPassport(passport));
+        if (accounts != null) {
+            for (Account account : accounts) {
+                if (account.getRequisite().equals(requisite)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account source = findByRequisite(srcPassport, srcRequisite);
+        Account destination = findByRequisite(destPassport, destRequisite);
+        if (source != null && destination != null) {
+            if (source.getBalance() >= amount) {
+                source.setBalance(source.getBalance() - amount);
+                destination.setBalance(destination.getBalance() + amount);
+                return true;
+            }
+        }
+        return false;
+    }
+}
