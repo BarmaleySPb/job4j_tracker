dc051b4e9522a8607a13eacf5c42ea75b899c184
Evgeny <e.g.zakharov@gmail.com>
2021-05-14 19:13:24 +0300

    2. Отсортировать департаменты [#519672]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 import java.util.List;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import static ru.job4j.collection.Departments.fillGaps;
 
 public class DepartmentsTest {
 
     @Test
     public void whenMissed() {
         List<String> input = Arrays.asList("k1/sk1", "k2", "k2/sk1/sk2/sk3");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/sk2", "k2/sk1/sk2/sk3");
-        List<String> result = fillGaps(input);
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/sk2",
+                "k2/sk1/sk2/sk3");
+        List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
@@ -21,24 +21,26 @@ public class DepartmentsTest {
     public void whenNonChange() {
         List<String> input = Arrays.asList("k1", "k1/sk1");
         List<String> expect = Arrays.asList("k1", "k1/sk1");
-        List<String> result = fillGaps(input);
+        List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void sortDesc() {
-        List<String> input = Arrays.asList("k2/sk1/sk2/sk3", "k4/sk1", "k1/sk1", "k2", "k2/sk1/sk2/sk5");
-        input = fillGaps(input);
+        List<String> input = Arrays.asList("k2/sk1/sk2/sk3", "k4/sk1", "k1/sk1", "k2",
+                "k2/sk1/sk2/sk5");
+        input = Departments.fillGaps(input);
         Departments.sortDesc(input);
-        List<String> expected = Arrays.asList("k4", "k4/sk1", "k2", "k2/sk1", "k2/sk1/sk2", "k2/sk1/sk2/sk3",
-                "k2/sk1/sk2/sk5", "k1", "k1/sk1");
+        List<String> expected = Arrays.asList("k4", "k4/sk1", "k2", "k2/sk1", "k2/sk1/sk2",
+                "k2/sk1/sk2/sk3", "k2/sk1/sk2/sk5", "k1", "k1/sk1");
         assertThat(input, is(expected));
     }
 
     @Test
     public void sortAsc() {
-        List<String> input = Arrays.asList("k2/sk1/sk2/sk3", "k4/sk1", "k1/sk1", "k2", "k2/sk1/sk2/sk5");
-        input = fillGaps(input);
+        List<String> input = Arrays.asList("k2/sk1/sk2/sk3", "k4/sk1", "k1/sk1", "k2",
+                "k2/sk1/sk2/sk5");
+        input = Departments.fillGaps(input);
         Departments.sortAsc(input);
         List<String> expected = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1", "k2/sk1/sk2",
                 "k2/sk1/sk2/sk3", "k2/sk1/sk2/sk5", "k4", "k4/sk1");
