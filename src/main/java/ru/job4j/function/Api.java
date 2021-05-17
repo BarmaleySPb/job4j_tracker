0c3eb4913d5c1bc0b3736bb4a7a5da7e7f3c4242
Evgeny <e.g.zakharov@gmail.com>
2021-05-17 16:45:00 +0300

    0. Stream API [#519513]

---
+++ src/main/java/ru/job4j/function/Api.java
@@ -0,0 +1,16 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Api {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 3, 4, -5, 7, -10, 34, 54, -45);
+        ArrayList<Integer> list = new ArrayList<>(numbers);
+        List<Integer> negNum = list.stream().filter(stream -> stream > 0).collect(Collectors.toList());
+        System.out.println(negNum);
+    }
+}
