7999e2051167a576c4e0b4354510e22c51eb57a4
Evgeny <e.g.zakharov@gmail.com>
2021-05-18 18:17:41 +0300

    3. Уникальность элементов и сортировка. [#519516]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,6 +6,10 @@ import java.util.stream.Collectors;
 public class Profiles {
 
         public static List<Address> collect(List<Profile> profile) {
-            return profile.stream().map(Profile::getAddress).collect(Collectors.toList());
+            return profile.stream()
+                    .map(Profile::getAddress)
+                    .sorted(new AddressSortedByCity())
+                    .distinct()
+                    .collect(Collectors.toList());
         }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -20,8 +20,25 @@ public class ProfilesTest {
         List<Address> result = Profiles.collect(profiles);
         List<Address> expected = new ArrayList<>();
         expected.add(new Address("Moscow", "Lenina", 12, 23));
-        expected.add(new Address("St. Peterbyrg", "Novikova", 122, 423));
         expected.add(new Address("Moscow", "Vtoraya sov", 62, 255));
+        expected.add(new Address("St. Peterbyrg", "Novikova", 122, 423));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void collectWhenDoubleAddress() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Lenina", 12, 23)),
+                new Profile(new Address("Moscow", "Lenina", 12, 23)),
+                new Profile(new Address("St. Peterbyrg", "Novikova", 122, 423)),
+                new Profile(new Address("St. Peterbyrg", "Novikova", 122, 423)),
+                new Profile(new Address("Moscow", "Vtoraya sov", 62, 255))
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Moscow", "Lenina", 12, 23));
+        expected.add(new Address("Moscow", "Vtoraya sov", 62, 255));
+        expected.add(new Address("St. Peterbyrg", "Novikova", 122, 423));
         assertThat(result, is(expected));
     }
 }
