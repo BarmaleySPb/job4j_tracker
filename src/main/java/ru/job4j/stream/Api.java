3acbadbce0607636c47a06aca0663a82c0925cb6
Evgeny <e.g.zakharov@gmail.com>
2021-05-21 21:18:29 +0300

    .

--- src/main/java/ru/job4j/stream/Api.java
+++ src/main/java/ru/job4j/stream/Api.java
@@ -10,7 +10,9 @@ public class Api {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 3, 4, -5, 7, -10, 34, 54, -45);
         ArrayList<Integer> list = new ArrayList<>(numbers);
-        List<Integer> negNum = list.stream().filter(stream -> stream > 0).collect(Collectors.toList());
+        List<Integer> negNum = list.stream()
+                .filter(stream -> stream > 0)
+                .collect(Collectors.toList());
         System.out.println(negNum);
     }
 }
